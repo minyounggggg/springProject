8f1f8bbed979ef3d8d05cdcba13df6d1e6ff74c6
minyounggggg <90227375+minyounggggg@users.noreply.github.com>
2024-07-04 02:36:57 +0900

    20240704_0236

--- src/main/java/com/spring/springProject/controller/MemberController.java
+++ src/main/java/com/spring/springProject/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.spring.springProject.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.springProject.service.MemberService;
+import com.spring.springProject.vo.MemberVO;
 
 @Controller
 @RequestMapping("/member")
@@ -21,7 +26,11 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value = "/memberLogin", method = RequestMethod.POST)
-	public String memberLoginPost(String mid) {
-		return "member/memberMain";
+	public String memberLoginPost(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+		String mid, String pwd, String idSave) {
+		
+		MemberVO vo = memberService.getMemberIdCheck(mid);
+		
+		return "member/memberLogin";
 	}
 }
--- src/main/java/com/spring/springProject/service/MemberServiceImpl.java
+++ src/main/java/com/spring/springProject/service/MemberServiceImpl.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.springProject.dao.MemberDAO;
+import com.spring.springProject.vo.MemberVO;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
 	@Autowired
 	MemberDAO memberDAO;
+
+	@Override
+	public MemberVO getMemberIdCheck(String mid) {
+		return memberDAO.getMemberIdCheck(mid);
+	}
 	
 }
