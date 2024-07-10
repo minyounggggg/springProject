22801253f4eb164d935750071101793f378779a2
minyoung <minmin0325@naver.com>
2024-07-10 16:15:15 +0900

    20240710_1615

--- src/main/java/com/spring/javaclassS15/controller/MemberController.java
+++ src/main/java/com/spring/javaclassS15/controller/MemberController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.spring.javaclassS15.service.MemberService;
 import com.spring.javaclassS15.vo.MemberVO;
